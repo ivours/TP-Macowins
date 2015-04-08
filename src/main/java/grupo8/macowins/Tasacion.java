70822263e1ef2f71703c57faf3aa77f660216d4c
Ivo Ursino <ursinoivo@gmail.com>
2015-04-07 22:36:56 -0300

    Agregados los dos test. Borradas las clases importado y nacional, se implemento todo en tasacion directamente.

--- src/main/java/grupo8/macowins/Tasacion.java
+++ src/main/java/grupo8/macowins/Tasacion.java
@@ -2,8 +2,18 @@ package grupo8.macowins;
 
 //Esto lo agregue para poner un tipo en la variable ORIGEN de la clase PRENDA
 
-public interface Tasacion {
-	public double tasa();
+public class Tasacion {
+	
+	private double Tasa;
+	
+	public double tasa() {
+		return Tasa;
+	}
+	
+	public void setTasa(double nuevaTasa) {
+		Tasa = nuevaTasa;
+		return;
+	}
+	
 }
 
-//hola
