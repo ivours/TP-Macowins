8af87216b072635652b7eb9108a86a5fb6e50ab0
Ivo Ursino <ursinoivo@gmail.com>
2015-04-15 00:22:46 -0300

    Eliminada la carpeta TPMacowins

--- src/main/java/grupo8/TPMacowins/Armani.java
+++
@@ -1,8 +0,0 @@
-package grupo8.TPMacowins;
-
-public class Armani implements Marca {
-	
-	public double recargo(int precioBase) {
-		return 1.65 * precioBase;
-	}
-}
--- src/main/java/grupo8/TPMacowins/Marca.java
+++
@@ -1,5 +0,0 @@
-package grupo8.TPMacowins;
-
-public interface Marca {
-	public double recargo(int precioBase);
-}
