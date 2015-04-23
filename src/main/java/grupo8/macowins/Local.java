29973bbf0f99cd8a67d1f5fddc4a45a3bd11c9df
felipecalvo <felipecalvo@yahoo.com.ar>
2015-04-23 16:33:58 -0300

    ejorados streams, no mas enies

--- src/main/java/grupo8/macowins/Local.java
+++ src/main/java/grupo8/macowins/Local.java
@@ -2,7 +2,7 @@ package grupo8.macowins;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class Local {
 	
@@ -17,15 +17,15 @@ public class Local {
 		ventas.add(unaVenta);
 	}
 	
-	public double gananciaDelDia(int año, int mes, int dia) {
+	public double gananciaDelDia(int anio, int mes, int dia) {
 		
-		return ventasDeLaFecha(año,mes,dia).stream().map(Venta::ganancia).reduce((venta1,venta2)-> venta1 + venta2).get();
+		return ventasDeLaFecha(anio,mes,dia).stream().map(Venta::ganancia).reduce((venta1,venta2)-> venta1 + venta2).get();
 		
 	}
 	
-	public List<Venta> ventasDeLaFecha(int año, int mes, int dia)
+	public List<Venta> ventasDeLaFecha(int anio, int mes, int dia)
 	{
-		LocalDate fecha = LocalDate.of(año, mes, dia);
-		return (List<Venta>) ventas.stream().filter(venta -> venta.esDeLaFecha(fecha));
+		LocalDate fecha = LocalDate.of(anio, mes, dia);
+		return ventas.stream().filter(venta -> venta.esDeLaFecha(fecha)).collect(Collectors.toList());
 	}
 }
--- src/main/java/grupo8/macowins/Venta.java
+++ src/main/java/grupo8/macowins/Venta.java
@@ -30,9 +30,9 @@ public class Venta {
 		return prendas.size();
 	}
 	//Constructor.
-	public Venta(List<Prenda> listaDePrendas, int año, int mes, int dia) {
+	public Venta(List<Prenda> listaDePrendas, int anio, int mes, int dia) {
 		
-		fecha = LocalDate.of(año,mes,dia);
+		fecha = LocalDate.of(anio,mes,dia);
 		prendas = listaDePrendas;
 		
 	}
